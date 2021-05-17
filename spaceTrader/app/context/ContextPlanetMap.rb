bd951c0f435a743be565ecfb841609bc7e1747f5
D Schaedler <dominicschaedler@gmail.com>
2021-05-17 11:52:38 -0400

    Draw a spaceship - Also cleaned up some operators - Spaceship does a nice orbit

--- spaceTrader/app/context/ContextPlanetMap.rb
+++ spaceTrader/app/context/ContextPlanetMap.rb
@@ -37,19 +37,32 @@ class ContextPlanetMap < Context
   def tick (args, planets)
     args.outputs.primitives << @staticOutput
 
-    args.state.shipFrame = args.state.tick_count.idiv(5).mod(3)
-    args.outputs.primitives << {x: args.inputs.mouse.x, y: args.inputs.mouse.y, w:32, h: 32, path: "sprites/spaceship#{args.state.shipFrame}.png", primitive_marker: :sprite}
+    if $game.sceneMain.planetSelect
+      planetSelect = $game.sceneMain.planetSelect
 
-    # We got math to do baby.
-    # We need to determine what degree around the planet the ship is.
-    args.state.shipDegree = args.state.tick_count.mod(360)
-    # Now we need to determine the location of the ship using triginometry
-    # Given a point, angle, and distance, determine a new point
-    args.outputs.primitives << [100, 100, "#{args.state.shipDegree}"].label
+      shipFrame = args.state.tick_count.idiv(5).mod(3)
+      shipDegree = args.state.tick_count.mod(360)
+      distance = 60
+
+      DEGREES_TO_RADIANS = Math::PI / 180
+
+      posX = (distance * Math.cos(shipDegree * DEGREES_TO_RADIANS)) + planetSelect.x
+      posY = distance * Math.sin(shipDegree * DEGREES_TO_RADIANS) + planetSelect.y
+
+      args.outputs.primitives << {x: posX, y: posY, w:32, h: 32, path: "sprites/spaceship#{shipFrame}.png", angle: shipDegree, primitive_marker: :sprite}
+    end
+
   end
 
   def checkPlanetSelect(args, planets)  
     for planet in planets
+
+      if $game.sceneMain.planetSelect
+        planetSelect = $game.sceneMain.planetSelect
+        
+        args.outputs.primitives << {x: planetSelect.x + 14, y: planetSelect.y - 4, text:planetSelect.name, r: 255, g:255, b:255, alignment_enum: 1, primitive_marker: :label}
+        args.outputs.primitives << {x: planetSelect.x - 2, y: planetSelect.y - 2, w: 32, h: 32, path: 'sprites/selectionCursor.png',primitive_marker: :sprite}
+      end
 
       if args.inputs.mouse.inside_rect? [planet.x, planet.y, 28, 28]
         args.outputs.primitives << {x: planet.x + 14, y: planet.y - 4, text:planet.name, r: 255, g:255, b:255, alignment_enum: 1, primitive_marker: :label}
@@ -57,10 +70,11 @@ class ContextPlanetMap < Context
         if args.inputs.mouse.up
           $game.sceneMain.planetMenu.destroyMenu(args)
           $game.sceneMain.planetSelect = planet
-          $game.sceneMain.planetMenu.createMenu(args, planet)
-          $game.sceneMain.context = :contextPlanetMenu
         end
       end
+
+      #$game.sceneMain.planetMenu.createMenu(args, planet)
+      #$game.sceneMain.context = :contextPlanetMenu
     end
   end
 end
