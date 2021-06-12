ad847d8bb9503c6dc9e635f5d9b183afe719dd1c
D Schaedler <dominicschaedler@gmail.com>
2021-06-12 13:57:51 -0400

    Removed mygame folder

--- mygame/app/utils.rb
+++
@@ -1,9 +0,0 @@
-# Returns a random number in a range of min, max. Not included in engine for some reason
-def randr (min, max) 
-  rand(max - min) + min
-end
-
-def hexToRGB (hexstring)
-  rgbArray = hexstring.gsub("#", "").split('').each_slice(2).map {|e| e.join.to_i(16)}
-  return rgbArray
-end
