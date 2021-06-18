67eb619838f878b0de70eaeb167de6c33f5a2bbb
D Schaedler <dominicschaedler@gmail.com>
2021-06-18 14:51:02 -0400

    Run some planet cycles before the player loads Simulate active economies and don't wait for the best planet price

--- spaceTrader/app/Game.rb
+++ spaceTrader/app/Game.rb
@@ -5,13 +5,19 @@ class Game
 
   # Runs once when game instance created
   def initialize args
-
+    @pregeneration = false
     @sceneMain = SceneMain.new(args)
     @scene = :main
   end
 
   # Main loop
-  def tick args
+  def tick(args)
+    if @pregeneration == false
+      200.times do
+        @sceneMain.cycle(args)
+      end
+      @pregeneration = true
+    end
     args.outputs.background_color = [0, 0, 0] # Set Engine background color to black. Makes Letterboxing Black.
     @sceneMain.tick(args)
 
