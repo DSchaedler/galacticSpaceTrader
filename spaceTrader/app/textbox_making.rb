8ba036d54657c86a8c5b9fac892ed65942166e34
D Schaedler <dominicschaedler@gmail.com>
2021-06-20 18:56:04 -0400

    uncompact this line

--- spaceTrader/app/textbox_making.rb
+++ spaceTrader/app/textbox_making.rb
@@ -2,7 +2,10 @@
 
 def textbox_background(x_pos:, y_pos:, width:, height:, segment: 32)
   y_pos -= segment
-  alpha = 255; r = 168; g = 181; b = 178
+  alpha = 255
+  r = 168
+  g = 181
+  b = 178
   textbox = []
   textbox << [x_pos, y_pos, segment, segment, 'sprites/textbox/textbox0.png', 0, alpha, r, g, b].sprite
   textbox << [x_pos + segment, y_pos, width - segment * 2, segment, 'sprites/textbox/textbox1.png', 0, alpha, r, g,
