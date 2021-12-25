9f309ab3a09ed3015ed08a41e72cf5337362b6e4
Dee Schaedler <dominicschaedler@gmail.com>
2021-12-25 11:14:18 -0500

    debugging

--- app/scene/scene_main.rb
+++ app/scene/scene_main.rb
@@ -143,7 +143,9 @@ class SceneMain < Scene
     case event
     when :supply_crash
       select_system = @systems.sample
+      puts @select_system
       select_planet = select_system.system_planets.sample
+      puts @select_planet
       select_material = select_planet.materials.sample
       puts select_material
       args.gtk.notify select_material.name
